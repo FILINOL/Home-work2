b4980e64891bee9d0dfdbc79f2522480c48c7de2
FILIN <hagi_beter@mail.ru>
2023-04-12 09:43:38 +0300

    Work01

---
+++ Work01/Program.cs
@@ -0,0 +1,5 @@
+﻿int number = new Random().Next(100, 999); 
+ Console.WriteLine("Случайное число: " + number);
+ int first = number / 10; // 45 / 10 == 4
+ int second = first % 10; // 45 % 10 == 5
+ Console.WriteLine("Число по середине: " + second);
