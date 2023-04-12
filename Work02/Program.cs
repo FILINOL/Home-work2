ebee97683ccf465d63d70a076d31f26f6ce0c7c9
FILIN <hagi_beter@mail.ru>
2023-04-12 10:19:02 +0300

    Work02

---
+++ Work02/Program.cs
@@ -0,0 +1,17 @@
+﻿int number = new Random().Next(10, 9999); 
+ Console.WriteLine("Случайное число: " + number);
+ int a = number % 10;
+ int first = number / 10; // 45 / 10 == 4
+ int second = first % 10; // 45 % 10 == 5
+
+if (number > 1000)
+ {
+     Console.WriteLine("Третья цифра: " + second);
+ }else if (number > 100)
+ {
+     Console.WriteLine("Третья цифра: " + a );
+ } else if (number < 100)
+ {
+    Console.WriteLine("Третьей цифры нет" );
+ }
+
